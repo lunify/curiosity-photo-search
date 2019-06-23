15d365c59f235f3c963e820293ea15dec2ca26a4
Anass Maddah <maddah.anass@gmail.com>
2019-06-23 21:01:34 +0100

    Added api data fetching in backend on post endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,41 @@
 var express = require('express')
 var morgan = require('morgan')
-var port = 3001
+var { port, nasaAPI } = require('./config')
 
 var app = express()
-
-app.use(express.json())
-app.use(morgan('combined'))
+app.use(express.json()).use(morgan('combined'))
 
 app.listen(port)
 
-app.get('/', function handle(req, res) {
-  res.json({ message: 'Hello world' })
+app.get('/', function sendGreetings(req, res) {
+  res.send({ message: 'Hello world' })
 })
+
+var axios = require('axios')
+var url = require('url')
+
+app.post('/', requestPhotos)
+
+
+// ******************************************
+
+function requestPhotos(req, res) {
+  var { query } = url.parse(req.url, true)
+  axios.get(`${nasaAPI.baseURL}?sol=${query.sol}&camera=${query.camera}&api_key=${nasaAPI.key}`)
+  .then(sendPhotoUrls)
+  .catch(sendError)
+
+
+  // *******************************
+  function sendError(err) {
+    res.status(500).send(err)
+  }
+
+  function sendPhotoUrls({ data }) {
+    res.send({ photoUrls: data.photos.map(getUrl) })
+  }
+}
+
+function getUrl(photo) {
+  return photo.img_src
+}
---
+++ server/src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 3001,
+  nasaAPI: {
+    baseURL: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos',
+    key: 'DEMO_KEY'
+  }
+}
