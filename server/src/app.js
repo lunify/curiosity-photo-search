17d5a81c021e96392a4ece89ae67aacd135f036d
Anass Maddah <maddah.anass@gmail.com>
2019-06-23 21:02:48 +0100

    Added basic param validation for the api request

--- server/src/app.js
+++ server/src/app.js
@@ -14,10 +14,24 @@ app.get('/', function sendGreetings(req, res) {
 var axios = require('axios')
 var url = require('url')
 
-app.post('/', requestPhotos)
+app.post('/', validateParams, requestPhotos)
 
 
 // ******************************************
+
+function validateParams(req, res, next) {
+  var { query } = url.parse(req.url, true)
+
+  if (!query.sol || !query.camera) {
+    return res.status(400).send({ message: 'Missing query param' })
+  }
+
+  if (Object.is(NaN, Number(query.sol)) || !Number.isInteger(Number(query.sol)) || query.sol < 0) {
+    return res.status(400).send({ message: 'Invalid sol value.' })
+  }
+
+  next()
+}
 
 function requestPhotos(req, res) {
   var { query } = url.parse(req.url, true)
